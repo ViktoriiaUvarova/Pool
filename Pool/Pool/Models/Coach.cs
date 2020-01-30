653f4cec63607f737f2bd578b37be16e7e273fcf
Viktoriia <uvarova.viktoriia01@gmail.com>
2020-01-30 23:04:06 +0200

    Create Models

---
+++ Pool/Pool/Models/Coach.cs
@@ -0,0 +1,12 @@
+﻿namespace Pool.Models
+{
+    public class Coach : Sportsman
+    {
+        public string ID_Treiner;
+
+        public override string ToString()
+        {
+            return $"Coach {base.ToString()}";
+        }
+    }
+}
---
+++ Pool/Pool/Models/Subscription.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pool.Models
+{
+    public class Subscription
+    {
+        public string ID_sportsmen;
+        public string ID_treiner;
+        public string data_oform;
+        public string srok;
+
+        public override string ToString()
+        {
+            return "ID Sportsmen:" + ID_sportsmen + " " + "ID Treiner: " + ID_treiner + " "
+                + "Oformlenie: " + data_oform + " " + "Srok: " + srok;
+        }
+    }
+}
